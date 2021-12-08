97dd2c481d01b57c38003a7779cea64b27c9b9e7
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2021-12-09 03:50:05 +0700

    Update

--- TruongTrungKien_Buoi6_ADR45/app/src/main/java/com/android45/truongtrungkien_buoi6_adr45/fragment/FragmentActivity.java
+++ TruongTrungKien_Buoi6_ADR45/app/src/main/java/com/android45/truongtrungkien_buoi6_adr45/fragment/FragmentActivity.java
@@ -1,7 +1,11 @@
 package com.android45.truongtrungkien_buoi6_adr45.fragment;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -26,6 +30,27 @@ public class FragmentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 getFragment(HomeFragment.newInstance());
+                activityFragmentBinding.imgHome.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN: {
+                                ImageView view = (ImageView) v;
+                                view.invalidate();
+                                view.getDrawable().setColorFilter(Color.BLACK, PorterDuff.Mode.MULTIPLY);
+                                break;
+                            }
+                            case MotionEvent.ACTION_UP:
+                            case MotionEvent.ACTION_CANCEL: {
+                                ImageView view = (ImageView) v;
+                                view.getDrawable().clearColorFilter();
+                                view.invalidate();
+                                break;
+                            }
+                        }
+                        return false;
+                    }
+                });
             }
         });
 
@@ -33,27 +58,139 @@ public class FragmentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 getFragment(MenuFragment.newInstance());
+                activityFragmentBinding.imgMenu.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN: {
+                                ImageView view = (ImageView) v;
+                                view.invalidate();
+                                view.getDrawable().setColorFilter(Color.BLACK, PorterDuff.Mode.MULTIPLY);
+                                break;
+                            }
+                            case MotionEvent.ACTION_UP:
+                            case MotionEvent.ACTION_CANCEL: {
+                                ImageView view = (ImageView) v;
+                                view.getDrawable().clearColorFilter();
+                                view.invalidate();
+                                break;
+                            }
+                        }
+                        return false;
+                    }
+                });
+            }
+        });
+
+        activityFragmentBinding.imgMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragment(MenuFragment.newInstance());
+                activityFragmentBinding.imgMenu.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN: {
+                                ImageView view = (ImageView) v;
+                                view.invalidate();
+                                view.getDrawable().setColorFilter(Color.BLACK, PorterDuff.Mode.MULTIPLY);
+                                break;
+                            }
+                            case MotionEvent.ACTION_UP:
+                            case MotionEvent.ACTION_CANCEL: {
+                                ImageView view = (ImageView) v;
+                                view.getDrawable().clearColorFilter();
+                                view.invalidate();
+                                break;
+                            }
+                        }
+                        return false;
+                    }
+                });
             }
         });
 
         activityFragmentBinding.imgCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                getFragment(MenuFragment.newInstance());
+                activityFragmentBinding.imgCart.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN: {
+                                ImageView view = (ImageView) v;
+                                view.invalidate();
+                                view.getDrawable().setColorFilter(Color.BLACK, PorterDuff.Mode.MULTIPLY);
+                                break;
+                            }
+                            case MotionEvent.ACTION_UP:
+                            case MotionEvent.ACTION_CANCEL: {
+                                ImageView view = (ImageView) v;
+                                view.getDrawable().clearColorFilter();
+                                view.invalidate();
+                                break;
+                            }
+                        }
+                        return false;
+                    }
+                });
             }
         });
 
         activityFragmentBinding.imgFavorite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                getFragment(MenuFragment.newInstance());
+                activityFragmentBinding.imgFavorite.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN: {
+                                ImageView view = (ImageView) v;
+                                view.invalidate();
+                                view.getDrawable().setColorFilter(Color.BLACK, PorterDuff.Mode.MULTIPLY);
+                                break;
+                            }
+                            case MotionEvent.ACTION_UP:
+                            case MotionEvent.ACTION_CANCEL: {
+                                ImageView view = (ImageView) v;
+                                view.getDrawable().clearColorFilter();
+                                view.invalidate();
+                                break;
+                            }
+                        }
+                        return false;
+                    }
+                });
             }
         });
 
         activityFragmentBinding.imgChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                getFragment(MenuFragment.newInstance());
+                activityFragmentBinding.imgChat.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN: {
+                                ImageView view = (ImageView) v;
+                                view.invalidate();
+                                view.getDrawable().setColorFilter(Color.BLACK, PorterDuff.Mode.MULTIPLY);
+                                break;
+                            }
+                            case MotionEvent.ACTION_UP:
+                            case MotionEvent.ACTION_CANCEL: {
+                                ImageView view = (ImageView) v;
+                                view.getDrawable().clearColorFilter();
+                                view.invalidate();
+                                break;
+                            }
+                        }
+                        return false;
+                    }
+                });
             }
         });
     }
--- TruongTrungKien_Buoi6_ADR45/app/src/main/java/com/android45/truongtrungkien_buoi6_adr45/fragment/MenuFragment.java
+++ TruongTrungKien_Buoi6_ADR45/app/src/main/java/com/android45/truongtrungkien_buoi6_adr45/fragment/MenuFragment.java
@@ -1,5 +1,7 @@
 package com.android45.truongtrungkien_buoi6_adr45.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.android45.truongtrungkien_buoi6_adr45.ImageConverter;
 import com.android45.truongtrungkien_buoi6_adr45.R;
 import com.android45.truongtrungkien_buoi6_adr45.databinding.MenuFragmentBinding;
 
@@ -29,6 +32,9 @@ public class MenuFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         menuFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.menu_fragment, container, false);
+//        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.salads);
+//        Bitmap circularBitmap = ImageConverter.getRoundedCornerBitmap(bitmap, 100);
+//        menuFragmentBinding.imgSalad.setImageBitmap(circularBitmap);
         return menuFragmentBinding.getRoot();
     }
 }
